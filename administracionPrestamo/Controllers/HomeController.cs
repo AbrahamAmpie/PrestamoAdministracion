0fd736fc0f6e8cf8a766df269520767fd8b29e84
Carlos Maradiaga <carlosmaradiaga777@gmail.com>
2022-09-24 18:18:29 -0600

    Add pantalla Login y Usuarios

--- administracionPrestamo/Controllers/HomeController.cs
+++ administracionPrestamo/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace administracionPrestamo.Controllers
             return View();
         }
 
+        public IActionResult Usuario()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
